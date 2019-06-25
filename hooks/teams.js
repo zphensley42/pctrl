12650b2f57781e571cd5ee295e8eaf280622f8e6
Zachary Hensley <zachary.hensley@siriusxm.com>
2019-06-25 16:07:20 -0500

    Updated hook to parse commands

--- hooks/teams.js
+++ hooks/teams.js
@@ -1,13 +1,42 @@
 'use strict';
 
 const crypto = require('crypto');
+const botName = 'Peeqo';
 
 class TeamsHook {
     constructor(res) {
         this.response = res;
 
-        this.sharedSecret = process.env.TEAMS_SEC;
-        this.bufSecret = Buffer(this.sharedSecret, "base64");
+        // this.sharedSecret = process.env.TEAMS_SEC;
+        // this.bufSecret = Buffer(this.sharedSecret, "base64");
+        this.commands = [
+            'Greeting',
+            'Camera',
+            'Timer',
+            'Weather',
+            'ChangeGlasses',
+            'Goodbye',
+            'KittyTime',
+            'HueGroup',
+            'HueLight',
+            'Patrick',
+            'GitHub'
+        ];
+    }
+
+    validCommand(cmd) {
+        return this.commands.indexOf(cmd) !== -1;
+    }
+
+    validateAuth(payload, request) {
+        // Retrieve authorization HMAC information
+        let auth = request.headers['authorization'];
+
+        // Calculate HMAC on the message we've received using the shared secret
+        let msgBuf = Buffer.from(payload, 'utf8');
+        let msgHash = "HMAC " + crypto.createHmac('sha256', this.bufSecret).update(msgBuf).digest("base64");
+
+        return msgHash === auth;
     }
 
     handle(request) {
@@ -18,22 +47,39 @@ class TeamsHook {
         };
         onDataFunc = onDataFunc.bind(this);
 
+        let validateAuthFunc = this.validateAuth;
+        validateAuthFunc = validateAuthFunc.bind(this);
+
+        let validCommandFunc = this.validCommand;
+        validCommandFunc = validCommandFunc.bind(this);
+
         let onEndFunc = function() {
             try {
-                // Retrieve authorization HMAC information
-                var auth = request.headers['authorization'];
-                // Calculate HMAC on the message we've received using the shared secret
-                var msgBuf = Buffer.from(payload, 'utf8');
-                var msgHash = "HMAC " + crypto.createHmac('sha256', this.bufSecret).update(msgBuf).digest("base64");
+                let validAuth = validateAuthFunc(payload, request);
 
-                if (msgHash === auth) {
-                    var receivedMsg = JSON.parse(payload);
-                    var responseMsg = '{ "type": "message", "text": "You typed: ' + receivedMsg.text +  ', have a gif: https://media.giphy.com/media/xU9TT471DTGJq/giphy.gif" }';
-                } else {
-                    var responseMsg = '{ "type": "message", "text": "Error: message sender cannot be authenticated." }';
+                let responseJson = {
+                    text: "Error: message sender cannot be authenticated."
+                };
+                if (validAuth) {
+                    let receivedMsg = JSON.parse(payload);
+                    console.log(receivedMsg);
+
+                    // Determine what command to send
+                    let receivedCommand = receivedMsg.text.replace(botName + ' ', '');
+                    if(validCommandFunc(receivedCommand)) {
+                        responseJson = {
+                            text: "Command found",
+                            command: `${receivedCommand}`
+                        };
+                    }
+                    else {
+                        responseJson = {
+                            text: "Command not found"
+                        };
+                    }
                 }
                 this.response.writeHead(200);
-                this.response.write(responseMsg);
+                this.response.write(JSON.stringify(responseJson));
                 this.response.end();
             }
             catch (err) {
